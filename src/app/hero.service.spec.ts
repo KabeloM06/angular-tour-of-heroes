49e710dd950de41a9f01c9bb42a53a83703b3067
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-06-09 10:08:16 +0200

    Unit testing practice: reset app component spec

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -10,7 +10,7 @@ describe('HeroService', () => {
     service = TestBed.inject(HeroService);
   });
 
-  it('should be created', () => {
+  /*it('should be created', () => {
     expect(service).toBeTruthy();
-  });
+  });*/
 });
